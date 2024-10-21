1003dc061cfa15d1da1dd325ef1f55193d3e721c
Mooogy <francismigsanjuan@gmail.com>
2024-10-21 03:05:06 -0700

    Implemented basic web requests to AniList API

--- src/components/AnimeSearch.tsx
+++ src/components/AnimeSearch.tsx
@@ -1,24 +1,6 @@
-import AniCard from "./AniCard";
-import { GraphQLClient, gql } from "graphql-request";
 import AniList from "./AniList";
 
-interface Anime {
-  id: number;
-  title: {
-    romaji: string;
-  };
-  coverImage: {
-    large: string;
-  };
-  episodes: number;
-}
-
-interface Props {
-  trendingAnime: Anime[];
-  currentSeasonAnime: Anime[];
-}
-
-const AnimeSearch = ({ trendingAnime, currentSeasonAnime }: Props) => {
+const AnimeSearch = () => {
   return (
     <div className="bg-foreground border-t-2 border-black h-screen flex justify-center items-center">
       <div className=" h-full w-2/3 flex flex-col gap-[1vh]">
